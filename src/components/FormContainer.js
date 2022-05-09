205591b64258beef84f243c4076e8c58a06a6de4
unknown <Mantashacode>
2022-05-09 14:06:45 +0530

    modify formContainer.js

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -6,19 +6,33 @@ import { useState } from "react";
 import { formSubmit } from "../redux/form/formActions";
 
 const FormContainer = () => {
-  const [firstName, setFirstName] = useState(null);
-  const [lastName, setLastName] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [pwd, setPwd] = useState(null);
-  const [pwd2, setPwd2] = useState(null);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [pwd2, setPwd2] = useState("");
 
   const dispatch = useDispatch();
+
+  const checkpwd = (e) => {
+    e.preventDefault();
+    if (pwd !== pwd2) alert("Password did not match");
+    else {
+      const data = { firstName, lastName, email, pwd };
+      dispatch(formSubmit(data));
+      setEmail("");
+      setFirstName("");
+      setLastName("");
+      setPwd("");
+      setPwd2("");
+    }
+  };
 
   return (
     <div className="main">
       <div className="main-container">
         <h4>Sign Up</h4>
-        <form className="row g-3">
+        <form className="row g-3" onSubmit={checkpwd}>
           <div className="col-md-6">
             <label for="firstName" class="form-label">
               Firstname
@@ -27,6 +41,7 @@ const FormContainer = () => {
               type="text"
               className="form-control"
               id="firstName"
+              value={firstName}
               onChange={(e) => {
                 setFirstName(e.target.value);
               }}
@@ -40,6 +55,7 @@ const FormContainer = () => {
               type="text"
               class="form-control"
               id="lastName"
+              value={lastName}
               onChange={(e) => {
                 setLastName(e.target.value);
               }}
@@ -53,6 +69,7 @@ const FormContainer = () => {
               type="email"
               class="form-control"
               id="inputEmail"
+              value={email}
               onChange={(e) => {
                 setEmail(e.target.value);
               }}
@@ -66,6 +83,7 @@ const FormContainer = () => {
               type="password"
               class="form-control"
               id="inputPassword"
+              value={pwd}
               onChange={(e) => {
                 setPwd(e.target.value);
               }}
@@ -79,24 +97,14 @@ const FormContainer = () => {
               type="password"
               class="form-control"
               id="inputPassword2"
+              value={pwd2}
               onChange={(e) => {
                 setPwd2(e.target.value);
               }}
             />
           </div>
           <div class="col-12">
-            <button
-              type="submit"
-              className="btn"
-              onClick={(e) => {
-                e.preventDefault();
-                if (pwd !== pwd2) alert("Password did not match");
-                else {
-                  const data = { firstName, lastName, email, pwd };
-                  dispatch(formSubmit(data));
-                }
-              }}
-            >
+            <button type="submit" className="btn">
               Sign in
             </button>
           </div>
